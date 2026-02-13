1a06fe95f50b33b4b0681cf542fee9afac8867e2
autistichna_riba <fizzexual@gmail.com>
2026-02-13 13:19:13 +0200

    v2.0.6: Fixed getIcon import in HomeScreen

--- app/src/main/java/com/velotravel/ui/home/HomeScreen.kt
+++ app/src/main/java/com/velotravel/ui/home/HomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import com.velotravel.data.model.Activity
 import com.velotravel.data.model.ActivityType
+import com.velotravel.data.model.getIcon
 import java.text.SimpleDateFormat
 import java.util.*
 
--- app/src/main/java/com/velotravel/update/UpdateChecker.kt
+++ app/src/main/java/com/velotravel/update/UpdateChecker.kt
@@ -24,7 +24,7 @@ class UpdateChecker(private val context: Context) {
     
     companion object {
         private const val GITHUB_API_URL = "https://api.github.com/repos/fizzexual/VeloTravel/releases/latest"
-        private const val CURRENT_VERSION = "2.0.5" // Use only basic Material icons that exist
+        private const val CURRENT_VERSION = "2.0.6" // Fixed getIcon import
     }
     
     suspend fun checkForUpdates(): UpdateInfo? = withContext(Dispatchers.IO) {
